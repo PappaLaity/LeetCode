cc7c2a661823b6e84a87afaadbea63eea709d6f5
Pappalaity <pappalaity@gmail.com>
2025-05-22 16:29:43 +0000

    test of two sum

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ from leetCode import LeetCode
 
 if __name__ == "__main__":
     leet_obj = LeetCode()
+
+    nums = [2,7,11,15]
+    target = 9
+
+    print(f"Calling twoSum function between {nums} and {target} give: \n")
+    result = leet_obj.twoSum(nums,target)
+    print(result)
