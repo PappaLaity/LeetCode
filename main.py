f1ad4e663470eafa937c1fd11d22c7439b4872e7
Pappalaity <pappalaity@gmail.com>
2025-05-22 16:34:07 +0000

    trying to use assert

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
     nums = [2,7,11,15]
     target = 9
 
-    print(f"Calling twoSum function between {nums} and {target} give: \n")
+    print(f"Calling twoSum function between {nums} and {target} give: ")
     result = leet_obj.twoSum(nums,target)
+    assert result == [0,1]
     print(result)
